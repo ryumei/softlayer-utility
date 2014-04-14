0ca8fb43a1de2e9419d9e00902ad46d12ce6756b
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2014-04-14 12:23:20 +0900

    Delete curl.sh
    move to rest-sample.sh

--- curl.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-# https://[username]:[apiKey]@api.[service.]softlayer.com/rest/v3/[serviceName]/[initializationParameter].[returnDatatype]
-
-curl https://${SL_USERNAME}:${SL_API_KEY}@api.softlayer.com/rest/v3/SoftLayer_Account.xml
