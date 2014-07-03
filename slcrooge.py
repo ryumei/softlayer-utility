881713ec16b640733a8ecc558f6a511ebf840e24
NAKAJIMA Takaaki <nakajima_t@worksap.co.jp>
2014-07-03 13:49:11 +0900

    add logging and BillingInvoices

--- slcrooge.py
+++ slcrooge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #                                        -*- coding:utf-8 -*-
 #
-#  slmon.py: get information utility
+#  slcrooge.py: Get Billing information utility
 #  Created by NAKAJIMA Takaaki 
 #  Last modified: Apr 16, 2014.
 #
@@ -19,9 +19,23 @@ import csv
 import datetime
 
 import SoftLayer
-client = SoftLayer.Client()
 
-logger = logging.getLogger()
+# ----------------------------------------------------------------------
+
+# Logging
+
+stream_log = logging.StreamHandler()
+stream_log.setLevel(logging.INFO)
+stream_log.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+
+file_log = logging.FileHandler(filename='slcrooge.log')
+file_log.setLevel(logging.DEBUG)
+file_log.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+
+logging.getLogger().addHandler(stream_log)
+logging.getLogger().addHandler(file_log)
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 # ----------------------------------------------------------------------
 
@@ -32,16 +46,23 @@ class IterableItems:
         self.master_account = client['Account']
         self.offset = 0
         self.limit = limit
+        self.mask = ''
         self.define_fetch_method()
-        self.fetched = self.fetch()
+        self.fetched = []
         
     def define_fetch_method(self):
         u"""MUST be implemented in inherited class"""
         # self.fetch_method に適切な pagenate メソッドを設定
-        raise NotImpementedError("Not implemented yet.")        
-    
+        raise NotImpementedError("Not implemented yet.")
+
+    #def set_mask(self, mask):
+    #    self.mask = mask
+
     def fetch(self):
-        items = self.fetch_method(limit=self.limit, offset=self.offset)
+        if (self.mask != ''):
+            items = self.fetch_method(limit=self.limit, offset=self.offset, mask=self.mask)
+        else:
+            items = self.fetch_method(limit=self.limit, offset=self.offset)
         self.offset += self.limit
         return items
     
@@ -50,27 +71,44 @@ class IterableItems:
     
     def __next__(self):
         if len(self.fetched) < 1:
-            raise StopIteration
-        item = self.fetched.pop()
-        if len(self.fetched) < 1:  # prefetch for next
             self.fetched = self.fetch()
+            logging.debug(self.__class__.__name__ + ' has been fetched.')
+            if len(self.fetched) < 1: # No redisual items
+                raise StopIteration
+        item = self.fetched.pop()
         return item
-    
+
 
 class Users(IterableItems):
-    u"""List of SoftLayer_User_Customer"""
+    u"""List of User_Customer"""
     def define_fetch_method(self):
         self.fetch_method = self.master_account.getUsers
 
 class VirtualGuests(IterableItems):
-    u"""List of SoftLayer_Virtual_Guest"""
+    u"""List of Virtual_Guest"""
     def define_fetch_method(self):
         self.fetch_method = self.master_account.getVirtualGuests
 
 class BillingItems(IterableItems):
-    u"""List of SoftLayer_Billing_Item"""
+    u"""List of Billing_Item"""
     def define_fetch_method(self):
         self.fetch_method = master_account.getAllBillingItems
+
+class BillingInvoices(IterableItems):
+    u"""List of Billing_Invoices"""
+    def __init__(self, client, limit=10):
+        super(BillingInvoices, self).__init__(client, limit)
+        self.mask = 'accountId,id,createDate,closedDate,modifyDate,startingBalance,endingBalance,taxStatusId,taxTypeId,statusCode,state,typeCode'
+
+    def define_fetch_method(self):
+        self.fetch_method = master_account.getInvoices
+
+import re
+def str2date(str):
+    # e.g. 2014-07-03T13:06:48+09:00 -> 2014-07-03T13:06:48+0900
+    str = re.sub(r'(\+\d\d):(\d\d)$', '\\1\\2', str)
+    return datetime.datetime.strptime(str, '%Y-%m-%dT%H:%M:%S%z')
+
 
 # --------------------------------------------------------------
 
@@ -123,11 +161,13 @@ FIELDNAMES=('id',
             )
 HEADER = dict([ (val,val) for val in FIELDNAMES ])  
 
-fname = 'slmon' + datetime.datetime.now().strftime("%y%m%d%H%M%S") + '.csv'
+fname = 'slcrooge-' + datetime.datetime.now().strftime("%y%m%d%H%M%S") + '.csv'
 
 # ----------------------------------------------------------------------
 
 try:
+    client = SoftLayer.Client()
+    
     master_account = client['Account']
     
     print("## Account information ##") 
@@ -135,25 +175,30 @@ try:
     account_info = master_account.getObject(mask=user_mask)
     print(account_info)
     
-    print("## Users ##");
-    for user in Users(client):
-        print("id:%d, %s" % (user['id'], user['username']))
+    #print("## Users ##");
+    #for user in Users(client, limit=20):
+    #    print("id:%d, %s" % (user['id'], user['username']))
 
-    print("## Billing items ##")
-    billingItems = []
-    for b in BillingItems(client):
-        billingItems.append(b)
-        #writer.writerow(b)
-        #print(json.dumps(b, sort_keys=True, indent=4))
-    
-    # Output
-    print('Write to ' + fname)
-    with open(fname, mode='w') as f:
-        writer = csv.DictWriter(f, FIELDNAMES, extrasaction='raise') # ignore/raise
-        billingItems.insert(0, HEADER)
-        writer.writerows(billingItems)
+    # https://%SL_USERNAME%:%SL_API_KEY%@api.softlayer.com/rest/v3/SoftLayer_Account/Invoices.json
+    print("Billing Invoices")
+    for i in BillingInvoices(client):
+        print(json.dumps(i, sort_keys=True, indent=2))
+        print(str2date(i['createDate']))
+
+#    print("Billing items: -> " + fname)
+#    billingItems = []
+#    for b in BillingItems(client):
+#        billingItems.append(b)
+#        #writer.writerow(b)
+#        #print(json.dumps(b, sort_keys=True, indent=4))
+#    
+#    # Output
+#    with open(fname, mode='w') as f:
+#        writer = csv.DictWriter(f, FIELDNAMES, extrasaction='raise') # ignore/raise
+#        billingItems.insert(0, HEADER)
+#        writer.writerows(billingItems)
 
 except SoftLayer.SoftLayerAPIError as e:
-    logger.error("Unable to retrieve account information faultCode%s, faultString=%s"
+    logging.error("Unable to retrieve account information faultCode=%s, faultString=%s"
           % (e.faultCode, e.faultString))
     exit(1)
